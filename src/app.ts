12b64784cfdcebfcedddd5035eefbf588ace2b36
1391020381 <1391020381@qq.com>
2021-10-18 16:13:13 +0800

    hello-ts-koa

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+const app = new Koa();
+
+const router = new Router();
+router.get('/',async(ctx,next)=>{
+    ctx.body = 'hello, ts-koa'
+})
+app.use(router.routes())
+app.listen(3000, '127.0.0.1', () => {
+    console.log('服务器在运行');
+})
